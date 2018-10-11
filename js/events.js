f8c0937d2314d23c38b1d8bfc7d8a6b5d44a0255
LOI PHAT DINH <liamphatdinh@gmail.com>
2018-10-11 02:09:44 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -13,12 +13,25 @@ function frameIt() {
 
 function pressIt() {
   $("form").on("keydown", function(key) {
-    if (key.which === )
+    if (key.which === 71) {
+      alert ("G was pressed.")
+    }
   })
+}
+
+function submitIt() {
+  $("form").on("submit", function(){
+    alert ("Your form is going to be submitted now.")
+  })
+}
 
 
 $(document).ready(function(){
 
 // call functions here
 getIt();
+framIt();
+pressIt();
+submitIt();
+
 });
